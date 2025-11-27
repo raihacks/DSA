41b001a5e7ee438cdab87edff7f1acf89bf0040f
raihacks <siddhirai326@gmail.com>
2025-11-27 19:08:04 +0530

    DSA start

---
+++ Main.java
@@ -0,0 +1,15 @@
+//O(1)
+// public class Main{
+//     public static void main(String[] args){
+//         int[] arr = {12,1,2,3,4,5};
+//         System.out.println(getFirstElement(arr));
+//     }
+//     public static int getFirstElement(int[] arr){
+//         return arr[0];
+//     }
+// }
+
+//O(log n)
+public class Main{
+
+}
