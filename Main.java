bfa8c0ffadd85bde662cc5c2993b73f967e4c1a3
raihacks <siddhirai326@gmail.com>
2025-11-27 19:51:26 +0530

    linearSearch

--- Main.java
+++ Main.java
@@ -11,30 +11,46 @@ import java.util.Arrays;
 // }
 
 //O(log n)
-public class Main{
-    public static void main(String[] args){
-        int[] arr = {12, 34, 56, 76, 89, 23, 46, 74};
-        int traget = 76;
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(binarySearch( arr, traget));
+// public class Main{
+//     public static void main(String[] args){
+//         int[] arr = {12, 34, 56, 76, 89, 23, 46, 74};
+//         int traget = 76;
+//         Arrays.sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//         System.out.println(binarySearch( arr, traget));
+//     }
+//     public static int binarySearch(int[] arr, int traget){
+//         int left = 0;
+//         int right = arr.length -1;
+//         while (left <= right){
+//             int mid =(left + right) / 2;
+//             if(arr[mid]==traget){
+//                 return mid;
+//             }
+//             else if(arr[mid] < traget){
+//                 left = mid + 1;
+//             }
+//             else{
+//                 right = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// }
 
-    }
-    public static int binarySearch(int[] arr, int traget){
-        int left = 0;
-        int right = arr.length -1;
-        while (left <= right){
-            int mid =(left + right) / 2;
-            if(arr[mid]==traget){
-                return mid;
-            }
-            else if(arr[mid] < traget){
-                left = mid + 1;
-            }
-            else{
-                right = mid - 1;
-            }
-        }
-        return -1;
-    }
-}
+//O(n)
+// public class Main{
+//     public static void main(String[] args){
+//         int[] arr = {12,67,98,0,23,56};
+//         int target = 23;
+//         System.out.println(linearSearch(arr,target));
+//     }
+//     public static int linearSearch(int[] arr,int target){
+//         for(int i = 0; i<arr.length; i++){
+//             if(arr[i] == target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
