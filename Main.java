d4405fc3569713dab5937caabdc72fb26ec3c92f
raihacks <siddhirai326@gmail.com>
2025-11-28 17:46:23 +0530

    leet Q1

--- Main.java
+++ Main.java
@@ -109,3 +109,17 @@ import java.util.Arrays;
 //         }
 //     }
 // }
+
+public class Main{
+    public static void main(String[] args){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i+1 ; j < nums.length; j++){
+                if(target == nums[i] + nums[j]){
+                    System.out.println("the indecis are " +i+ " and "+j);
+                }
+            }    
+        }
+    }
+}
