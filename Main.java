9b5a7bff1ac0e9c2220c881d8726e20ce363e08b
raihacks <siddhirai326@gmail.com>
2025-12-02 09:37:12 +0530

    Move Zeroes to End

--- Main.java
+++ Main.java
@@ -228,8 +228,24 @@ import java.util.Arrays;
 //     }
 // }
 
-public class Main{
-    public static void main(String[] args){
-        
-    }
-}
+// public class Main {
+//     public static void moveZeroes(int[] nums) {
+//         int insertPos = 0;
+//         for (int num : nums) {
+//             if (num != 0) {
+//                 nums[insertPos++] = num;
+//             }
+//         }
+//         while (insertPos < nums.length) {
+//             nums[insertPos++] = 0;
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int[] arr = {0, 1, 0, 3, 12};
+//         moveZeroes(arr);
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//     }
+// }
+
